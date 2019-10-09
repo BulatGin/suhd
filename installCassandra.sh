4e5ca2b43af80b497f043d40dd37d5e19864f326
Bulat Giniyatullin <bulat.giniiatullin@gmail.com>
2019-10-09 23:13:00 +0300

    Add script for cassandra installation

---
+++ installCassandra.sh
@@ -0,0 +1,28 @@
+echo "-------INSTALL JAVA-------"
+
+sudo apt-get install -y openjdk-8-jre-headless
+
+echo "-----JAVA VERSION CHECK------"
+java -version
+
+echo "----------INSTALL PYTHON 2----------"
+
+sudo apt-get install -y python-minimal
+
+echo "------PYTHON VERSION CHECK------"
+python --version
+
+echo "-----------INSTALL CASSANDRA---------"
+
+if ! ([ -f /etc/apt/sources.list.d/cassandra.sources.list] && grep -Fq "deb http://www.apache.org/dist/cassandra/debian 36x main" /etc/apt/sources.list.d/cassandra.sources.list); then
+    echo "deb http://www.apache.org/dist/cassandra/debian 36x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+fi
+
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install -y cassandra
+
+echo "------TEST RUN------"
+nodetool status
