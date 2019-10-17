ad29b77995ea55deafb6113bb29e9d0a2db9f228
Bulat Giniyatullin <bulat.giniiatullin@gmail.com>
2019-10-17 22:24:06 +0300

    Fix path to setNodesIps

--- cassandra/installAndConfCassandraCluster.sh
+++ cassandra/installAndConfCassandraCluster.sh
@@ -6,7 +6,7 @@ for i in {1..3}
 do
     node=NODE$i
     nodeLocal=NODE${i}_LOC
-    scp -i ~/.ssh/AWSCourses.pem ./setNodesIps.sh ubuntu@${!node}:~/.
+    scp -i ~/.ssh/AWSCourses.pem ../setNodesIps.sh ubuntu@${!node}:~/.
     if [ "$option" != "--only-conf" ]; then
         ssh -i ~/.ssh/AWSCourses.pem ubuntu@${!node} "bash -s" -- < ./installCassandra.sh
     fi
